2fd11cf868f5e9d6118a2222b0de9adb1036a7fa
georgehao <haohongfan@gmail.com>
2024-12-04 20:03:12 +0800

    update

--- crates/scroll/chainspec/src/scroll.rs
+++ crates/scroll/chainspec/src/scroll.rs
@@ -3,8 +3,7 @@
 use alloc::sync::Arc;
 
 use alloy_chains::{Chain, NamedChain};
-use alloy_eips::eip1559::ETHEREUM_BLOCK_GAS_LIMIT;
-use alloy_primitives::{b256, U256};
+use alloy_primitives::b256;
 use reth_chainspec::{once_cell_set, ChainSpec};
 use reth_scroll_forks::ScrollHardfork;
 
@@ -22,10 +21,7 @@ pub static SCROLL_MAINNET: LazyLock<Arc<ScrollChainSpec>> = LazyLock::new(|| {
             genesis_hash: once_cell_set(b256!(
                 "bbc05efd412b7cd47a2ed0e5ddfcf87af251e414ea4c801d78b6784513180a80"
             )),
-            paris_block_and_final_difficulty: Some((0, U256::from(0))),
             hardforks: ScrollHardfork::scroll_mainnet(),
-            max_gas_limit: ETHEREUM_BLOCK_GAS_LIMIT,
-            prune_delete_limit: 10000,
             ..Default::default()
         },
     }
