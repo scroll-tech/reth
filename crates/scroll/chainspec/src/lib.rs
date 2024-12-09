a188c1b4cdbb97d4c928d789454a8ee9f6be525d
georgehao <haohongfan@gmail.com>
2024-12-09 17:48:15 +0800

    update the doc

--- crates/scroll/chainspec/src/lib.rs
+++ crates/scroll/chainspec/src/lib.rs
@@ -1,6 +1,10 @@
 //! Scroll-Reth chain specs.
 
-#![doc = include_str!("../../../../docs/hardforks.md")]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/paradigmxyz/reth/main/assets/reth-docs.png",
+    html_favicon_url = "https://avatars0.githubusercontent.com/u/97369466?s=256",
+    issue_tracker_base_url = "https://github.com/paradigmxyz/reth/issues/"
+)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
 #![cfg_attr(not(feature = "std"), no_std)]
 
