ecba636993efeeb8bc9db2d39f2fe1c1e0fe0fa8
Gregory Edison <gregory.edison1993@gmail.com>
2024-12-11 10:52:58 +0100

    fix: remove `conflicts_with` on bin args
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>

--- crates/scroll/cli/src/args.rs
+++ crates/scroll/cli/src/args.rs
@@ -6,10 +6,10 @@ use reth_node_builder::engine_tree_config::{
 #[derive(Debug, clap::Args)]
 pub struct ScrollRollupArgs {
     /// Configure persistence threshold for engine experimental.
-    #[arg(long = "engine.persistence-threshold", conflicts_with = "legacy", default_value_t = DEFAULT_PERSISTENCE_THRESHOLD)]
+    #[arg(long = "engine.persistence-threshold", default_value_t = DEFAULT_PERSISTENCE_THRESHOLD)]
     pub persistence_threshold: u64,
 
     /// Configure the target number of blocks to keep in memory.
-    #[arg(long = "engine.memory-block-buffer-target", conflicts_with = "legacy", default_value_t = DEFAULT_MEMORY_BLOCK_BUFFER_TARGET)]
+    #[arg(long = "engine.memory-block-buffer-target", default_value_t = DEFAULT_MEMORY_BLOCK_BUFFER_TARGET)]
     pub memory_block_buffer_target: u64,
 }
