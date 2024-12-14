bbda0dd9764a03cead12baa6ced3a8091ab64dce
Gregory Edison <gregory.edison1993@gmail.com>
2024-12-14 11:44:42 +0100

    fix: clippy
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>

--- crates/primitives/src/transaction/compat.rs
+++ crates/primitives/src/transaction/compat.rs
@@ -13,7 +13,7 @@ pub trait FillTxEnv {
 impl FillTxEnv for TransactionSigned {
     fn fill_tx_env(&self, tx_env: &mut TxEnv, sender: Address) {
         #[cfg(any(feature = "optimism", feature = "scroll"))]
-        let envelope = alloy_eips::eip2718::Encodable2718::encoded_2718(self);
+        let _envelope = alloy_eips::eip2718::Encodable2718::encoded_2718(self);
 
         tx_env.caller = sender;
         match self.as_ref() {
@@ -105,7 +105,7 @@ impl FillTxEnv for TransactionSigned {
                     source_hash: Some(tx.source_hash),
                     mint: tx.mint,
                     is_system_transaction: Some(tx.is_system_transaction),
-                    enveloped_tx: Some(envelope.into()),
+                    enveloped_tx: Some(_envelope.into()),
                 };
                 return;
             }
@@ -133,14 +133,14 @@ impl FillTxEnv for TransactionSigned {
                 source_hash: None,
                 mint: None,
                 is_system_transaction: Some(false),
-                enveloped_tx: Some(envelope.into()),
+                enveloped_tx: Some(_envelope.into()),
             }
         }
 
         #[cfg(all(feature = "scroll", not(feature = "optimism")))]
         if !self.is_l1_message() {
             tx_env.scroll.is_l1_msg = false;
-            tx_env.scroll.rlp_bytes = Some(envelope.into());
+            tx_env.scroll.rlp_bytes = Some(_envelope.into());
         }
     }
 }
