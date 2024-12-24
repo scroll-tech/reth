1d62191ee4763c2f83860f41eaa281c5721948d2
Gregory Edison <gregory.edison1993@gmail.com>
2024-12-24 18:06:50 +0100

    feat: feature gate state root verification
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>

--- crates/stages/stages/src/stages/merkle.rs
+++ crates/stages/stages/src/stages/merkle.rs
@@ -275,6 +275,9 @@ where
         // Reset the checkpoint
         self.save_execution_checkpoint(provider, None)?;
 
+        #[cfg(feature = "scroll-mpt")]
+        let _ = trie_root;
+        #[cfg(not(feature = "scroll-mpt"))]
         validate_state_root(trie_root, SealedHeader::seal(target_block), to_block)?;
 
         Ok(ExecOutput {
