087a6fc0a769319d164d450c1e6bffb27679038d
Gregory Edison <gregory.edison1993@gmail.com>
2025-01-03 13:10:58 +0100

    chore: lint
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>

--- crates/scroll/bin/scroll-reth-mpt/src/main.rs
+++ crates/scroll/bin/scroll-reth-mpt/src/main.rs
@@ -1,15 +1,15 @@
 //! Scroll binary
 
-use clap::Parser;
-use reth_node_builder::{engine_tree_config::TreeConfig, EngineNodeLauncher};
-use reth_provider::providers::BlockchainProvider2;
-use reth_scroll_cli::{Cli, ScrollChainSpecParser, ScrollRollupArgs};
-use reth_scroll_node::{ScrollAddOns, ScrollNodeMpt};
-
 #[global_allocator]
 static ALLOC: reth_cli_util::allocator::Allocator = reth_cli_util::allocator::new_allocator();
 
+#[cfg(all(feature = "scroll", not(feature = "optimism")))]
 fn main() {
+    use clap::Parser;
+    use reth_node_builder::{engine_tree_config::TreeConfig, EngineNodeLauncher};
+    use reth_provider::providers::BlockchainProvider2;
+    use reth_scroll_cli::{Cli, ScrollChainSpecParser, ScrollRollupArgs};
+    use reth_scroll_node::{ScrollAddOns, ScrollNodeMpt};
     reth_cli_util::sigsegv_handler::install();
 
     // Enable backtraces unless a RUST_BACKTRACE value has already been explicitly provided.
@@ -42,4 +42,10 @@ fn main() {
         eprintln!("Error: {err:?}");
         std::process::exit(1);
     }
+}
+
+#[cfg(any(feature = "optimism", not(feature = "scroll")))]
+fn main() {
+    eprintln!("Scroll feature is not enabled");
+    std::process::exit(1);
 }
