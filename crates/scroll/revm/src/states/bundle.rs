eef1281ce7ed3fdac6cac139f8d32a29ccbcafaf
lightsing <light.tsing@gmail.com>
2025-01-07 10:12:09 +0800

    FIXME: remove gates

--- crates/scroll/revm/src/states/bundle.rs
+++ crates/scroll/revm/src/states/bundle.rs
@@ -72,7 +72,7 @@ impl From<(BundleState, &ScrollPostExecutionContext)> for ScrollBundleState {
 }
 
 // This conversion can cause a loss of information since performed without additional context.
-#[cfg(any(not(feature = "scroll"), feature = "test-utils"))]
+// #[cfg(any(not(feature = "scroll"), feature = "test-utils"))]
 impl From<BundleState> for ScrollBundleState {
     fn from(bundle: BundleState) -> Self {
         (bundle, &ScrollPostExecutionContext::default()).into()
