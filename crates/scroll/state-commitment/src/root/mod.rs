5e42fa0354181292111cae21bf19617a1b63ed77
lightsing <light.tsing@gmail.com>
2025-01-08 16:19:37 +0800

    gate out reth-provider and reth-trie-parallel

--- crates/scroll/state-commitment/src/root/mod.rs
+++ crates/scroll/state-commitment/src/root/mod.rs
@@ -20,7 +20,9 @@ use tracing::{debug, trace};
 #[cfg(feature = "metrics")]
 use reth_trie::metrics::{StateRootMetrics, TrieRootMetrics, TrieType};
 
+#[cfg(feature = "parallel")]
 mod parallel;
+#[cfg(feature = "parallel")]
 pub use parallel::ParallelStateRoot;
 
 mod utils;
