67bf3bac6777b91b2a9306982026a20b0c28abf6
frisitano <francesco.risitano95@gmail.com>
2025-04-10 18:32:54 +0700

    pin alloy-evm dep

--- crates/scroll/alloy/evm/src/block/mod.rs
+++ crates/scroll/alloy/evm/src/block/mod.rs
@@ -214,10 +214,6 @@ where
     fn evm_mut(&mut self) -> &mut Self::Evm {
         &mut self.evm
     }
-
-    fn evm(&self) -> &Self::Evm {
-        &self.evm
-    }
 }
 
 /// An extension of the [`Evm`] trait for Scroll.
