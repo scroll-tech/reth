345de575adc0de45b896caf50c92e74d1b8cacbf
frisitano <francesco.risitano95@gmail.com>
2025-04-22 11:58:44 +0700

    fix: set non-zero base fee

--- crates/scroll/evm/src/config.rs
+++ crates/scroll/evm/src/config.rs
@@ -111,7 +111,7 @@ where
             gas_limit: attributes.gas_limit,
             // calculate basefee based on parent block's gas usage
             // TODO(scroll): update with correct block fee calculation for block building.
-            basefee: parent.base_fee_per_gas().unwrap_or_default(),
+            basefee: 100,
             blob_excess_gas_and_price: None,
         };
 
