71bf48947b5648cc000116ffa233e44ae53bffe2
Gregory Edison <gregory.edison1993@gmail.com>
2025-05-05 11:04:05 +0200

    fix: lints
    Signed-off-by: Gregory Edison <gregory.edison1993@gmail.com>

--- crates/consensus/consensus/src/lib.rs
+++ crates/consensus/consensus/src/lib.rs
@@ -11,7 +11,11 @@
 
 extern crate alloc;
 
-use alloc::{fmt::Debug, string::String, vec::Vec};
+use alloc::{
+    fmt::Debug,
+    string::{String, ToString},
+    vec::Vec,
+};
 use alloy_consensus::Header;
 use alloy_primitives::{BlockHash, BlockNumber, Bloom, B256};
 use reth_execution_types::BlockExecutionResult;
