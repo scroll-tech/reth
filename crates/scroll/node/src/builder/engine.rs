225cfab125fea5a3e35139afa0d5d3c8947f1904
frisitano <francesco.risitano95@gmail.com>
2025-05-21 13:17:44 +0400

    feat: refactor engine payload validation

--- crates/scroll/node/src/builder/engine.rs
+++ crates/scroll/node/src/builder/engine.rs
@@ -125,6 +125,15 @@ impl PayloadValidator for ScrollEngineValidator {
         // First parse the block
         let mut block = try_into_block(payload, self.chainspec.clone())?;
 
+        // Seal the block with the pre-existing difficulty and return if hashes match.
+        let block_hash = block.hash_slow();
+        if block_hash == expected_hash {
+            return block
+                .seal_unchecked(block_hash)
+                .try_recover()
+                .map_err(|err| NewPayloadError::Other(err.into()));
+        }
+
         // Seal the block with the no-turn difficulty and return if hashes match.
         // We guess the difficulty, which should always be 1 or 2 on Scroll.
         // CLIQUE_NO_TURN_DIFFICULTY is used starting at Euclid, so we test this value first.
