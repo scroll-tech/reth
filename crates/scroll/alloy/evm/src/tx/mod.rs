44e039244732f7b1d205479f273e724b606d3344
Péter Garamvölgyi <peter@scroll.io>
2025-06-27 11:07:37 +0200

    update compute compression ratio

--- crates/scroll/alloy/evm/src/tx/mod.rs
+++ crates/scroll/alloy/evm/src/tx/mod.rs
@@ -160,8 +160,8 @@ impl FromTxWithEncoded<ScrollTxEnvelope> for ScrollTransactionIntoTxEnv<TxEnv> {
         };
 
         let encoded = (!tx.is_l1_message()).then_some(encoded);
-        let compression_ratio = encoded.as_ref().map(compute_compression_ratio);
-        Self::new(base, encoded, compression_ratio)
+        let compression_ratio = compute_compression_ratio(base.input());
+        Self::new(base, encoded, Some(compression_ratio))
     }
 }
 
@@ -268,7 +268,7 @@ impl FromRecoveredTx<ScrollTxEnvelope> for ScrollTransactionIntoTxEnv<TxEnv> {
         };
 
         let rlp_bytes = (!tx.is_l1_message()).then_some(envelope.into());
-        let compression_ratio = rlp_bytes.as_ref().map(compute_compression_ratio);
-        Self::new(base, rlp_bytes, compression_ratio)
+        let compression_ratio = compute_compression_ratio(base.input());
+        Self::new(base, rlp_bytes, Some(compression_ratio))
     }
 }
