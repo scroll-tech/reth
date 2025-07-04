18e0db6a5f80ffb66a290b683eda20296fa12e01
Morty <yiweichi1@gmail.com>
2025-07-05 01:28:10 +0800

    refactor: remove fields on ScrollNode

--- crates/scroll/node/src/node.rs
+++ crates/scroll/node/src/node.rs
@@ -16,16 +16,11 @@ use reth_trie_db::MerklePatriciaTrie;
 
 /// The Scroll node implementation.
 #[derive(Clone, Debug, Default)]
-pub struct ScrollNode {
-    /// A bool that represents if the transaction broadcast should be disabled.
-    pub disable_tx_broadcast: bool,
-    /// A bool that represents if the transaction receiving should be disabled.
-    pub disable_tx_receive: bool,
-}
+pub struct ScrollNode;
 
 impl ScrollNode {
     /// Returns a [`ComponentsBuilder`] configured for a regular Ethereum node.
-    pub fn components<Node>(&self) -> ComponentsBuilder<
+    pub fn components<Node>() -> ComponentsBuilder<
         Node,
         ScrollPoolBuilder,
         BasicPayloadServiceBuilder<ScrollPayloadBuilderBuilder>,
@@ -74,7 +69,7 @@ where
     >;
 
     fn components_builder(&self) -> Self::ComponentsBuilder {
-        Self::components(self)
+        Self::components()
     }
 
     fn add_ons(&self) -> Self::AddOns {
