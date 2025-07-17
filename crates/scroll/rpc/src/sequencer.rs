4572e311c4cb310ba6e8cf1a4732eeea3d5d71a4
Morty <yiweichi1@gmail.com>
2025-07-17 17:29:02 +0800

    fix: ci fmt

--- crates/scroll/rpc/src/sequencer.rs
+++ crates/scroll/rpc/src/sequencer.rs
@@ -155,10 +155,7 @@ mod tests {
 
         let request = client
             .client()
-            .make_request(
-                "eth_sendRawTransaction",
-                format!("0x{}", hex::encode("abcd")),
-            )
+            .make_request("eth_sendRawTransaction", format!("0x{}", hex::encode("abcd")))
             .serialize()
             .unwrap()
             .take_request();
@@ -190,10 +187,7 @@ mod tests {
 
         let request = client
             .client()
-            .make_request(
-                "eth_sendRawTransaction",
-                format!("0x{}", hex::encode("abcd")),
-            )
+            .make_request("eth_sendRawTransaction", format!("0x{}", hex::encode("abcd")))
             .serialize()
             .unwrap()
             .take_request();
