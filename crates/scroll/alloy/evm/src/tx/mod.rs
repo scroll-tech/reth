4a78eaad381f0bed5004bc88051eb5596569bd09
parizval <anmolgoyal@gmail.com>
2025-09-26 02:12:38 +0530

    perf: skipping computing compression ratio for l1 messages

--- crates/scroll/alloy/evm/src/tx/mod.rs
+++ crates/scroll/alloy/evm/src/tx/mod.rs
@@ -161,8 +161,9 @@ impl FromTxWithEncoded<ScrollTxEnvelope> for ScrollTransactionIntoTxEnv<TxEnv> {
 
         let encoded = (!tx.is_l1_message()).then_some(encoded);
         // Note: We compute the transaction ratio on tx.data, not on the full encoded transaction.
-        let compression_ratio = compute_compression_ratio(base.input());
-        Self::new(base, encoded, Some(compression_ratio))
+        let compression_ratio =
+            (!tx.is_l1_message()).then_some(compute_compression_ratio(base.input()));
+        Self::new(base, encoded, compression_ratio)
     }
 }
 
@@ -270,7 +271,9 @@ impl FromRecoveredTx<ScrollTxEnvelope> for ScrollTransactionIntoTxEnv<TxEnv> {
 
         let encoded = (!tx.is_l1_message()).then_some(envelope.into());
         // Note: We compute the transaction ratio on tx.data, not on the full encoded transaction.
-        let compression_ratio = compute_compression_ratio(base.input());
-        Self::new(base, encoded, Some(compression_ratio))
+        let compression_ratio =
+            (!tx.is_l1_message()).then_some(compute_compression_ratio(base.input()));
+
+        Self::new(base, encoded, compression_ratio)
     }
 }
