fe5f52c0b3f41acef6cc8d6eb360f4e6db69c2cc
frisitano <francesco.risitano95@gmail.com>
2025-11-27 12:30:55 +0400

    set galileo timestamps

--- crates/scroll/alloy/hardforks/src/hardfork.rs
+++ crates/scroll/alloy/hardforks/src/hardfork.rs
@@ -41,7 +41,8 @@ impl ScrollHardfork {
             (Self::Euclid, ForkCondition::Timestamp(1744815600)),
             (Self::EuclidV2, ForkCondition::Timestamp(1745305200)),
             (Self::Feynman, ForkCondition::Timestamp(1755576000)),
-            (Self::Galileo, ForkCondition::Timestamp(u64::MAX)),
+            (Self::Galileo, ForkCondition::Timestamp(1762506265)),
+            // TODO: update GalileoV2 timestamp when devnet is prepared
             (Self::GalileoV2, ForkCondition::Timestamp(u64::MAX)),
         ]
     }
