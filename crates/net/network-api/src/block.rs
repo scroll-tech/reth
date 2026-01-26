5e21e5208f1c8d21f819e1c15bb3fb4cbc832ec9
Morty <yiweichi1@gmail.com>
2026-01-26 10:51:47 +0800

    update comment

--- crates/net/network-api/src/block.rs
+++ crates/net/network-api/src/block.rs
@@ -24,6 +24,6 @@ pub trait EthWireProvider<N: NetworkPrimitives> {
     /// Announce a new block to the network over the eth wire protocol.
     fn eth_wire_announce_block(&self, block: N::NewBlockPayload, hash: B256);
 
-    /// Announce a new block to the network over the eth wire protocol.
+    /// Announce a new block to a specific peer over the eth wire protocol.
     fn eth_wire_announce_block_to(&self, peer_id: PeerId, block: N::NewBlockPayload, hash: B256);
 }
